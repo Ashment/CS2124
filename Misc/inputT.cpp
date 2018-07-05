a31e4cc77be361c30e9a1512eafb8e2fd436aee3
Ashment <gundamme@gmail.com>
2018-07-05 12:19:13 -0400

    Trying Inputs.

---
+++ Misc/inputT.cpp
@@ -0,0 +1,9 @@
+#include "std_lib_facilities.h"
+
+int main(){
+	cout << "Please Enter Name:\n";
+	string first_name;
+	cin >> first_name;
+	first_name+="\n";
+	cout << "Hello " << first_name;
+}
---
+++ Misc/inputT2.cpp
@@ -0,0 +1,12 @@
+#include "std_lib_facilities.h"
+
+int main(){
+	cout << "Please Enter Name:\n";
+	string first_name;
+	int age;
+	cin >> first_name;
+	cout << "Please Enter Age:\n";
+	cin >> age;
+	cout << "Hello " << first_name << "\n";
+	cout << "Age: " << age << "\n";
+}
