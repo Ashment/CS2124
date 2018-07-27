c14c101e79826b681fa268e2694ecbfc18f16989
Ashment <gundamme@gmail.com>
2018-07-27 14:46:54 +0000

    Begin work on date testing interface.

--- HW5/DateT.cpp
+++ HW5/DateT.cpp
@@ -13,4 +13,25 @@ int main(){
 	cout << "Enter Date(YYYY/MM/DD): ";
 	cin >> today;
 	cout << today;
+
+    cout << "Interact with Date class here.\n"
+        << "type 'h' for help.\n";
+    char cmd;
+    while(cin){
+        cout << "|> ";
+        cin >> cmd;
+        switch(cmd){
+            case 'h':
+                PrintHelp();
+                break;
+       }
+    }
+
+}
+
+void PrintHelp(){
+    cout << endl
+        << "Below are commands for interacting with Date.\n"
+        << "h : help\n" << "p : print date\n" << "s : set date\n"
+        << "d : add days\n" << "m : add months\n" << "y : add years\n"
 }
