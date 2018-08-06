43ea6703b90f7edfa9c55a2494a72925d14ceeed
Ashment <gundamme@gmail.com>
2018-08-06 03:33:56 +0000

    Fixed mem_test.cpp

--- MemEx/mem_test.cpp
+++ MemEx/mem_test.cpp
@@ -4,9 +4,14 @@ int main(){
     int i = 1;
     int *p;
     while(true){
-        std::cout << "iteration: " << i << endl;
-        p = new int[i];
-        i+=1;
+        try{
+            p = new int[10000 * i];
+            i+=1;
+        }catch(const std::exception& e){
+            std::cout << "Iteration: " << i << std::endl;
+            std::cout << "Error: " << e.what() << std::endl;
+            return 0;
+        }
     }
     return 0;
 }
